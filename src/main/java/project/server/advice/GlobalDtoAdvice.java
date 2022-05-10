54cad6cad1e85e5ced4056269891bf8ef95cc328
happysubin <asb0711@gmail.com>
2022-05-11 00:06:16 +0900

    feat : DTO Validation 추가2

--- src/main/java/project/server/advice/GlobalDtoAdvice.java
+++ src/main/java/project/server/advice/GlobalDtoAdvice.java
@@ -1,2 +1,35 @@
-package project.server.advice;public class GlobalDtoAdvice {
+package project.server.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.server.config.ErrorCode;
+import project.server.config.ErrorResponse;
+import project.server.exception.member.MemberNotFoundException;
+
+import java.util.Locale;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalDtoAdvice {
+
+    private final MessageSource ms;
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse MemberNotFoundException(MethodArgumentNotValidException exception){
+        BindingResult bindingResult = exception.getBindingResult();
+
+        ObjectError error = bindingResult.getAllErrors().get(0);
+        String code = error.getCodes()[0];
+
+        return new ErrorResponse(ErrorCode.DTO_NOT_FIT_SPEC, HttpStatus.BAD_REQUEST, ms.getMessage(code,null,null));
+    }
 }
--- src/main/java/project/server/advice/MemberAdvice.java
+++ src/main/java/project/server/advice/MemberAdvice.java
@@ -3,14 +3,12 @@ package project.server.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import project.server.config.ErrorResponse;
 import project.server.exception.member.MemberNotFoundException;
 
 @RestControllerAdvice
-public class UserAdvice {
-
+public class MemberAdvice {
 
     @ExceptionHandler(MemberNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
