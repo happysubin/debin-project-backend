ef7cbb144c8826c58943bf2eb38ecef0fc898cdc
happysubin <asb0711@gmail.com>
2022-05-13 01:05:33 +0900

    feat : MemberRepository findByNickName 메서드 추가

--- src/main/java/project/server/repository/member/MemberRepository.java
+++ src/main/java/project/server/repository/member/MemberRepository.java
@@ -1,11 +1,19 @@
 package project.server.repository.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project.server.domain.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+
+    @Query("select m from Member m where m.nickName = :nickName ")
+    public Optional<Member> findByNickName(String nickName);
 
 
 
